fdb3b78a0da7812349ef1c97bd2840ed6fee0c99
Supriya Sharma <sharmasup565@gmail.com>
2022-03-25 00:18:34 -0700

    refactor existing code

--- develop/script.js
+++ develop/script.js
@@ -1,4 +1,6 @@
-console.log("connected!")
+// declare & initialize global variables
+var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
 init();
 
 function init() {
@@ -6,65 +8,78 @@ function init() {
     $("#currentDay").text(currDay);
 
     makeTimeBlocks();
-
     setPastFuture();
 }
 
-function makeTimeBlocks () {
-    for (i = 9; i <= 17; i++) {
-
-        // TODO simplify
-        var hour;
-        if (i >= 13) {
-            hour = i - 12;
-        } else {
-            hour = i;
-        }
-        if (i < 12) {
-            hour += "AM";
-        } else {
-            hour += "PM";
-        }
-
+function makeTimeBlocks() {
+    for (i = 0; i < workHours.length; i++) {
+        
         var timeBlockEl = $("<div>");
         timeBlockEl.addClass("row");
         timeBlockEl.addClass("time-block");
+
         var hourEl = $("<label>");
+        var hourText;
+        if (workHours[i] > 12) {
+            hourText = workHours[i] - 12;
+        } else {
+            hourText = workHours[i];
+        }
+        if (workHours[i] >= 12) {
+            hourText += "PM";
+        } else {
+            hourText += "AM";
+        }
         hourEl.addClass("hour");
-        hourEl.text(hour);
+        hourEl.text(hourText);
+
         var textEl = $("<textarea>");
         textEl.addClass("description");
+
         var buttonEl = $("<button>");
         buttonEl.addClass("saveBtn");
+        buttonEl.attr("hour", workHours[i]);
 
         timeBlockEl.append(hourEl);
         timeBlockEl.append(textEl);
         timeBlockEl.append(buttonEl);
 
         $(".container").append(timeBlockEl);
+
     }
 }
 
-/* <div class="row time-block">
-        <label class="hour">9AM</label>
-        <textarea class="description"></textarea>
-        <button class="saveBtn"></button>
-</div> */
-
 function setPastFuture() {
     var currHour = moment().format("H");
-
     var timeBlocks = $(".container").children();
 
-    var timeBlockHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-
     for (i = 0; i < timeBlocks.length; i++) {
-        if (timeBlockHours[i] == currHour) {
+        if (currHour == workHours[i]) {
             timeBlocks.eq(i).children().eq(1).addClass("present");
-        } else if (timeBlockHours[i] < currHour) {
+        } else if (currHour > workHours[i]) {
             timeBlocks.eq(i).children().eq(1).addClass("past");
         } else {
             timeBlocks.eq(i).children().eq(1).addClass("future");
         }
     }
 }
+
+
+
+
+// $(".saveBtn").on("click", function(event) {
+//     console.log(event.target);
+
+// use hour attr - 9 for indexing
+// })
+
+
+
+
+
+
+// /* <div class="row time-block">
+//         <label class="hour">9AM</label>
+//         <textarea class="description"></textarea>
+//         <button class="saveBtn"></button>
+// </div> */
